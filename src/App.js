841b4e071bd0af9685b4801e843886f84228ec99
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-26 21:10:52 -0700

    added map method for day of month select

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function App() {
     sessionStorage.setItem('year', e.target.value);
   }
 
+  const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
   const today = new Date().toLocaleDateString();
 
   // const targetDate = new Date('April 29, 2020')
@@ -61,38 +63,8 @@ function App() {
           <option value="December">December</option>
         </select>
         <select onChange={handleDayChange} defaultValue={sessionStorage.day || date.getDate()}>
-          <option value='1'>01</option>
-          <option value='2'>02</option>
-          <option value='3'>03</option>
-          <option value='4'>04</option>
-          <option value='5'>05</option>
-          <option value='6'>06</option>
-          <option value='7'>07</option>
-          <option value='8'>08</option>
-          <option value='9'>09</option>
-          <option value='10'>10</option>
-          <option value='11'>11</option>
-          <option value='12'>12</option>
-          <option value='13'>13</option>
-          <option value='14'>14</option>
-          <option value='15'>15</option>
-          <option value='16'>16</option>
-          <option value='17'>17</option>
-          <option value='18'>18</option>
-          <option value='19'>19</option>
-          <option value='20'>20</option>
-          <option value='21'>21</option>
-          <option value='22'>22</option>
-          <option value='23'>23</option>
-          <option value='24'>24</option>
-          <option value='25'>25</option>
-          <option value='26'>26</option>
-          <option value='27'>27</option>
-          <option value='28'>28</option>
-          <option value='29'>29</option>
-          <option value='30'>30</option>
-          <option value='31'>31</option>
-        </select>
+          {days.map(day => <option value={day}>{day}</option>)}
+         </select>
         <input type="text" defaultValue={sessionStorage.year || date.getFullYear()} onChange={handelYearChange} maxLength="4" />
         <h3>{targetDate.toLocaleDateString()} is {numDay} days from now</h3>
       </div>
